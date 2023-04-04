c7928ffe4de7d75954adb4afba2f4182c191ceb3
Craig0547 <124737579+Craig0547@users.noreply.github.com>
2023-04-04 12:20:00 +0200

    Update LineFuncRoot.java

--- LineFuncRoot.java
+++ LineFuncRoot.java
@@ -1,21 +1,27 @@
 import java.util.Scanner;
+import java.util.Locale;
 
-class LineFunc {
-    public static void main(String[] args){
-    float Center, Length,
-    Root=0,
-    a=2,
-    b=1,
-    LeftMargin=-100,
-    RightMargin=100;
-    Scanner scanner = new Scanner(System.in);
-    
-    while((a*Root+b)!=0){
-        Length = RightMargin-LeftMargin;
-        Center = (Length)/2;
-        if((a*Root+b)>0) RightMargin=Center;
-        if((a*Root+b)<0) LeftMargin=Center;
-    }
-    System.out.println(Root);
+public class LineFunc
+{
+    public static void main(String[] args)
+    {
+        double Root = 0,
+        RightMargin=-1000,
+        LeftMargin=100,
+        Precision=0.01;
+        
+        while(Math.abs(RightMargin-LeftMargin)>=Precision)
+        {
+            Root=(RightMargin+LeftMargin)/2;
+            if((2*Root+1)<=0)
+            {
+                Root=RightMargin;
+            }
+            else
+            {
+                Root=LeftMargin;
+            }
+        }
+        System.out.println(Root);
     }
 }
